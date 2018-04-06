164d5d0270bf2edfce2cdddc93d067d823ae4825
phowson <phil@howson.net>
2018-04-06 15:44:50 +0100

    Basic UI skeleton

---
+++ ui/src/firebase/firebase.js
@@ -0,0 +1,8 @@
+const config = {
+  apiKey: "AIzaSyDAEDD5moOkblTYHZvIG-jCMTl-FRbuWaw",
+    authDomain: "pisecurity-72796.firebaseapp.com",
+    databaseURL: "https://pisecurity-72796.firebaseio.com",
+    projectId: "pisecurity-72796",
+    storageBucket: "pisecurity-72796.appspot.com",
+    messagingSenderId: "229435963846"
+};
