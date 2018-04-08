7b78d172be3b3a32d95257bb5f701ce94c9656e1
phowson <phil@howson.net>
2018-04-08 14:45:50 +0100

    Adding more basic layout

--- ui/src/components/Navigation.js
+++ ui/src/components/Navigation.js
@@ -3,14 +3,28 @@ import { Link } from 'react-router-dom';
 
 import * as routes from '../constants/routes';
 
-const Navigation = () =>
+import SignOutButton from './SignOut';
+
+const Navigation = ({ authUser }) =>
   <div>
-    <ul>
-      <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
-      <li><Link to={routes.LANDING}>Landing</Link></li>
-      <li><Link to={routes.HOME}>Home</Link></li>
-      <li><Link to={routes.ACCOUNT}>Account</Link></li>
-    </ul>
+    { authUser
+        ? <NavigationAuth />
+        : <NavigationNonAuth />
+    }
   </div>
+
+const NavigationAuth = () =>
+  <ul>
+    <li><Link to={routes.LANDING}>Landing</Link></li>
+    <li><Link to={routes.HOME}>Home</Link></li>
+    <li><Link to={routes.ACCOUNT}>Account</Link></li>
+    <li><SignOutButton /></li>
+  </ul>
+
+const NavigationNonAuth = () =>
+  <ul>
+    <li><Link to={routes.LANDING}>Landing</Link></li>
+    <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
+  </ul>
 
 export default Navigation;
--- ui/src/firebase/firebase.js
+++ ui/src/firebase/firebase.js
@@ -1,3 +1,6 @@
+
+
+import * as firebase from 'firebase';
 const config = {
   apiKey: "AIzaSyDAEDD5moOkblTYHZvIG-jCMTl-FRbuWaw",
     authDomain: "pisecurity-72796.firebaseapp.com",
@@ -5,4 +8,14 @@ const config = {
     projectId: "pisecurity-72796",
     storageBucket: "pisecurity-72796.appspot.com",
     messagingSenderId: "229435963846"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+const auth = firebase.auth();
+
+export {
+  auth,
 };
