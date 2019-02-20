ccaa8b8de00ab53259dafc3e04f551284441bdaf
phil <phil@PHILSPC>
2019-02-20 17:06:09 +0000

    Update settings

--- ui/src/firebase/firebase.js
+++ ui/src/firebase/firebase.js
@@ -3,7 +3,7 @@
 import * as firebase from 'firebase';
 const config = {
   apiKey: "AIzaSyDAEDD5moOkblTYHZvIG-jCMTl-FRbuWaw",
-    authDomain: "pisecurity-72796.firebaseapp.com",
+    authDomain: "pisecurity-72797.firebaseapp.com",
     databaseURL: "https://pisecurity-72796.firebaseio.com",
     projectId: "pisecurity-72796",
     storageBucket: "pisecurity-72796.appspot.com",
